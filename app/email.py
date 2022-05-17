a715ec90fb34ced73ca228053b4a0933a8dcd74d
davidmusembi <davidmusembi02@gmail.com>
2022-05-17 05:47:18 +0300

    fix a bug in email

--- app/email.py
+++ app/email.py
@@ -8,4 +8,5 @@
 #     email = Message(subject, sender=sender_email, recipients=[to])
 #     email.body= render_template(template + ".txt",**kwargs)
 #     email.html = render_template(template + ".html",**kwargs)
+
 #     mail.send(email)
